7bac6005938bcaa987360118727c0e7077fecfe3
wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>
2023-08-21 16:23:23 -0300

    Resolvendo Desafio 4
    Signed-off-by: wkyoshimatsu <101765683+wkyoshimatsu@users.noreply.github.com>

---
+++ src/stream_api/Desafio4.java
@@ -0,0 +1,23 @@
+package stream_api;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio4 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3); //NÃO CONSEGUI ALTERAR ESSA LISTA
+        /*
+        Desafio 4 - Remova todos os valores ímpares:
+        Utilize a Stream API para remover os valores ímpares da lista e imprima a lista resultante no console.
+        */
+        System.out.println("Desafio 3");
+        List<Integer> numerosCopiados = new ArrayList<Integer>(numeros);
+        //numerosCopiados.remove(1);
+        List<Integer> imparesParaRemover = numerosCopiados.stream()
+                .filter(n -> (n % 2 != 0))
+                .toList();
+        numerosCopiados.removeAll(imparesParaRemover);
+        System.out.println(numerosCopiados);
+    }
+}
